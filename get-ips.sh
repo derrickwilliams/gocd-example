b3a0e4229991a22fb9ce387ae25121e7cfe310cb
Derrick W <derrickawilliams@gmail.com>
2017-04-03 07:18:28 -0400

    Merging fix

--- get-ips.sh
+++ get-ips.sh
@@ -1,7 +1,6 @@
 CONTAINER_NAME=$1
 
-
 GOCD_SERVER_IP=`docker inspect --format='{{(index (index .NetworkSettings.IPAddress))}}' $CONTAINER_NAME`
 GOCD_SERVER_PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8153/tcp") 0).HostPort}}' $CONTAINER_NAME`
 
-echo "SERVER - ${GOCD_SERVER_IP:-'unknown'}:${GOCD_SERVER_PORT}"
+echo "SERVER - ${GOCD_SERVER_IP:-'host'}:${GOCD_SERVER_PORT}"
